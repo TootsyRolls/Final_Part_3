c0e8f43ede62c21012669ebd7f130c1ab7f67d4b
TootsyRolls <tonygian812@gmail.com>
2024-12-02 23:24:34 -0500

    WIP 01/12/24

--- src/main/java/AverageDriver/AverageDriver.java
+++ src/main/java/AverageDriver/AverageDriver.java
@@ -1,7 +1,10 @@
 package AverageDriver;
 
+import Classes.Average;
+
 public class AverageDriver {
     public static void main(String[] args) {
-
+        Average avg = new Average();
+        System.out.println(avg);
     }
 }
--- src/main/java/Classes/Average.java
+++ src/main/java/Classes/Average.java
@@ -3,43 +3,53 @@ package Classes;
 import java.util.Scanner;
 
 public class Average {
+    // Array and mean data fields
     private int[] data;
     private double mean;
 
     Scanner input = new Scanner(System.in);
 
     public Average() {
-        data = new int[5];
+        data = new int[5]; // Array Size
+        // For loop to input 5 values
         for (int i = 0; i < 5; i++) {
-            System.out.println("Please enter a score: ");
+            System.out.println("Please enter score " + (i+1) + ": ");
             data[i] = input.nextInt();
 
-            selectionSort();
-            calculateMean();
+            selectionSort(); // Sorts values into descending order
+            calculateMean(); // Finds middle value
         }
     }
+
     public void selectionSort() {
+        // For Loop to sort max -> min values
         for (int i = 0; i < 5 - 1; i++) {
             int max = i;
-            for (int j = i + 1; j < 5; j++) {
+            for (int j = i + 1; j < 5; j++) { // Checks if data at j > max
                 if (data[j] > data[max]) {
-                    max = j;
+                    max = j; // If so then replace data at j is new max
                 }
             }
-            int temp = data[max];
-            data[max] = data[i];
-            data[i] = temp;
+            int temp = data[max]; // Value at the index of data[max] is passed on temp
+            data[max] = data[i]; // Value at the index of data[i] is passed on max
+            data[i] = temp; // Value of temp replaces data[i]
         }
     }
 
     public void calculateMean() {
         int sum = 0;
         for (int score : data) {
-            sum += score;
+            sum += score; // For loop that adds all scores
         }
-        mean = (double) sum / 5;
+        mean = (double) sum / 5; // Gives mean
     }
 
     public String toString() {
+        String ans = "Score in descending order: ";
+        for (int score : data) {
+            ans += score + " ";
+        }
+        ans += "\nMean: " + String.format("%.2f", mean); // Adds new line Mean: XX.XX
+        return ans;
     }
 }
